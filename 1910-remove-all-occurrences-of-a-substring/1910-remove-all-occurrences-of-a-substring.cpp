9c447f1ed803d19e6fbd4d187a7ec8ccbde530e9
Hariharanreddy <82924830+Hariharanreddy@users.noreply.github.com>
2022-01-14 21:21:36 +0530

    Time: 0 ms (100.00%), Space: 6.5 MB (85.41%) - LeetHub

--- 1910-remove-all-occurrences-of-a-substring/1910-remove-all-occurrences-of-a-substring.cpp
+++ 1910-remove-all-occurrences-of-a-substring/1910-remove-all-occurrences-of-a-substring.cpp
@@ -4,7 +4,7 @@ public:
         
         int len = part.length();
         
-        while(s.size()!=0 && s.find(part) < s.length())
+        while(s.length()!=0 && s.find(part) < s.length())
         {
             s.erase(s.find(part), len);
         }
