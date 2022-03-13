975955abd27293cbe054414cce7234390be50031
Hariharanreddy <82924830+Hariharanreddy@users.noreply.github.com>
2022-03-13 11:47:25 +0530

    Added solution - LeetHub

--- Circular tour - GFG/circular-tour.cpp
+++ Circular tour - GFG/circular-tour.cpp
@@ -24,33 +24,42 @@ class Solution{
   
     //Function to find starting point where the truck can start to get through
     //the complete circle without exhausting its petrol in between.
-    int tour(petrolPump p[],int n)
+    int tour(petrolPump arr[],int n)
     {
-       int start = 0;
-       int deficit = 0;
-       int bal = 0;       //balance
-       
-       for(int i=0 ; i<n ; i++)
-       {
-           bal += p[i].petrol - p[i].distance;
+    int start = 0; 
+    int end = 1; 
+
+    int curr_petrol = arr[start].petrol - arr[start].distance; 
+
+    /* Run a loop while all petrol pumps are not visited. 
+    And we have reached first petrol pump again with 0 or more petrol */
+    while (end != start || curr_petrol < 0) 
+    { 
+        // If current amount of petrol in truck becomes less than 0, then 
+        // remove the starting petrol pump from tour 
+        while (curr_petrol < 0 && start != end) 
+        { 
+            // Remove starting petrol pump. Change start 
+            curr_petrol -= arr[start].petrol - arr[start].distance; 
+            start = (start + 1) % n; 
+
+            // If 0 is being considered as start again, then there is no 
+            // possible solution 
+             if (start == 0) 
+            return -1; 
            
-           //if bal gets negative -> reset bal = 0, move start, add deficit
-           if(bal < 0)
-           {
-               deficit += bal;
-               bal = 0;
-               start = i+1;
-           }
-       }
-       
-       if(deficit + bal >= 0)
-       {
-           return start;
-       }
-       else
-       return -1;
-       
-    }
+        } 
+
+        // Add a petrol pump to current tour 
+        curr_petrol += arr[end].petrol - arr[end].distance; 
+        end = (end + 1) % n; 
+    } 
+
+    // Return starting point 
+    return start; 
+} 
+
+
 };
 
 
