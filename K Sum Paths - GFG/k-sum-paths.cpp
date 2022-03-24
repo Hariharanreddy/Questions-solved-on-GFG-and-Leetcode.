b5252e36fec91e427874d1b9b302bf01b842639e
Hariharanreddy <82924830+Hariharanreddy@users.noreply.github.com>
2022-03-24 11:53:30 +0530

    Added solution - LeetHub

--- K Sum Paths - GFG/k-sum-paths.cpp
+++ K Sum Paths - GFG/k-sum-paths.cpp
@@ -99,40 +99,40 @@ struct Node
 */
 class Solution{
   public:
-    void solve(Node *root, int k, vector<int> vec, int &count)
+    void solve(Node *root, int k, vector<int> path, int &count)
     {
         //base case
         if(root == NULL)
-        {
             return;
-        }
+            
+        path.push_back(root -> data);
         
-        vec.push_back(root -> data);
+        solve(root -> left, k, path, count);
+        solve(root -> right, k, path, count);
         
-        solve(root -> left, k, vec,  count);
-        solve(root -> right, k, vec,  count);
-        
-        int size = vec.size();
         int sum = 0;
-        for(int i = size-1 ; i>=0 ; i--)
+        for(int i = path.size() - 1 ; i>= 0 ; i--)
         {
-            sum = sum + vec[i];
+            sum = sum+path[i];
             if(sum == k)
             {
                 count++;
             }
         }
         
-        vec.pop_back();
+        //while returning pop 
+        path.pop_back();
     }
     
     int sumK(Node *root,int k)
     {
-        vector<int> vec;
-        int count = 0;
-        
-        solve(root, k, vec, count);
-        return count;
+         vector<int> path;
+         int count = 0;
+         
+         solve(root, k, path, count);
+         
+         return count;
+         
         
     }
 };
