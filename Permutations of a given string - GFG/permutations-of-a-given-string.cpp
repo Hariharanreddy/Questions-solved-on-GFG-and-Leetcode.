e61d6bfda7181cbf4b19f8ece792c744c234cc20
Hariharanreddy <82924830+Hariharanreddy@users.noreply.github.com>
2022-04-17 19:48:06 +0530

    Added solution - LeetHub

--- Permutations of a given string - GFG/permutations-of-a-given-string.cpp
+++ Permutations of a given string - GFG/permutations-of-a-given-string.cpp
@@ -7,68 +7,27 @@ class Solution
 {
 	public:
 	
-	   // void solve(string s, vector<string>& ans, int index)
-	   // {
-	   //     //base case
-	   //     if(index >= s.length())
-	   //     {
-	   //         ans.push_back(s);
-	   //         return;
-	   //     }
+	    void solve(string s, vector<string>& ans, int index)
+	    {
+	        //base case
+	        if(index == s.length()-1)
+	        {
+	            ans.push_back(s);
+	            return;
+	        }
 	        
-	   //     for(int j=index ; j<s.length() ; j++)
-	   //     {
-	   //         swap(s[index] , s[j]);
-	   //         solve(s, ans, index+1);
+	        for(int j=index ; j<s.length() ; j++)
+	        {
+	            swap(s[index] , s[j]);
+	            solve(s, ans, index+1);
 	            
-	   //         //backtracking
-	   //         swap(s[index], s[j]);
-	   //     }
-	   // }
+	            //backtracking
+	            swap(s[index], s[j]);
+	        }
+	    }
 	   
 	   
-	   
-	   
-	   
-	   
-	   
-	   
-	   
-	   
-	   
-	   
-	   
-	   
-	   
-	   
-	   
-	   
-	   
-	   
-	   
-	   
-	   void solve(string S, vector<string> &ans, int index)
-	   {
-	       //base case
-	       if(index == S.length())
-	       {
-	           ans.push_back(S);
-	           return;
-	       }
-	       
-	       for(int i=index; i<S.length(); i++)
-	       {
-	           swap(S[index], S[i]);
-	           solve(S, ans, index+1);
-	           swap(S[index], S[i]);            //backtracking
-	           //solve(S, ans, index+1);
-	           
-	       }
-	       
-	   }
-	   
-	    
-		vector<string>find_permutation(string S)
+		vector<string> find_permutation(string S)
 		{
 		    // Code here there
 		    vector<string> ans;
