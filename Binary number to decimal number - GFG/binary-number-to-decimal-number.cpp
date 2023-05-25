483c0a284e1c370488d0c1958b27388cf5b90695
S. Hariharan Reddy <82924830+Hariharanreddy@users.noreply.github.com>
2023-05-25 12:31:59 +0530

    Added solution - LeetHub

--- Binary number to decimal number - GFG/binary-number-to-decimal-number.cpp
+++ Binary number to decimal number - GFG/binary-number-to-decimal-number.cpp
@@ -7,7 +7,16 @@ class Solution {
   public:
     int binary_to_decimal(string str) {
         // Code here.
-        return stoi(str, 0, 2);
+        int dec = 0;
+        int n = str.length();
+        int j = 0;
+        
+        for(int i=n-1 ; i >= 0 ; i--){
+            dec = ((str[i] - '0')*(pow(2, j))) + dec;
+            j++;
+        }
+        
+        return dec;
     }
 };
 
